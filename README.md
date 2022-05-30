f178075ad8549e14b3e9a25e46e0e59ffa2c8fbc
gre99ory <greggory@free.fr>
2022-05-30 21:55:15 +0200

    Day 6 - Neural Network

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Day 2 - Road 00:45:00
 Day 3 - Sensors 01:10:00
 Day 4 - Collision Detection 01:24:00
 Day 5 - Simulating Traffic 01:35:30
+Day 6 - Neural Network 02:03:00
--- car.js
+++ car.js
@@ -13,8 +13,17 @@ class Car {
         this.angle=0;
         this.damaged=false;
 
+        this.useBrain = controlType == "AI";
+
         if (controlType!="DUMMY") {
             this.sensor=new Sensor(this);
+            this.brain = new NeuralNetwork(
+                // 3 couches 
+                //      Senseurs
+                //      Couche Cachee
+                //      4 directions
+                [this.sensor.rayCount,6,4]
+            );
         }
 
         this.controls=new Controls(controlType);
@@ -28,6 +37,18 @@ class Car {
         }
         if (this.sensor) {
             this.sensor.update(roadBorders,traffic);
+            // 1 = On est tout pres
+            const offsets = this.sensor.readings.map(
+                s => s == null ? 0 : 1 - s.offset 
+            );
+            const outputs = NeuralNetwork.feedForward(offsets,this.brain);
+
+            if ( this.useBrain ) {
+                this.controls.forward = outputs[0];
+                this.controls.left    = outputs[1];
+                this.controls.right   = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
         }
     }
 
